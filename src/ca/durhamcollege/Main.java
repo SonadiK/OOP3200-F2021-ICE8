2a64493b062e9de8c36c2cd40882876f0342c6e6
Sonadi Kannangara <ksonadi@gmail.com>
2021-11-08 22:52:03 -0500

    Basic input and output test

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,21 @@
+/**
+ * Author: Sonadi Kannangara
+ * Date  : November 11, 2021
+ */
 package ca.durhamcollege;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String name = null;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+        System.out.printf("You entered:  %s", name);
+
     }
 }
